5b0fe314c4f981f5f6f92615cac24c99145c6da2
Rodrigo Naranjo González <56093078+rng1@users.noreply.github.com>
2022-04-20 23:52:54 +0200

    formatting

--- p2.c
+++ p2.c
@@ -9,7 +9,6 @@ int main(int argc, char *argv[])
     int i, done = 0, n, count;
     double PI25DT = 3.141592653589793238462643;
     double pi, x, y, z, piAux;
-
     int numprocs, rank;
 
     MPI_Status status;
@@ -18,47 +17,35 @@ int main(int argc, char *argv[])
     MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-
-
-    if (rank == 0){
-
+    if (rank == 0) {
         printf("Enter the number of points: (0 breaks) ");
         scanf("%d",&n);
-
     }
 
     MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
     
-
-    if (n == 0) return 0;
+    if (n == 0) 
+        return 0;
 
     count = 0;  
 
     for (i = rank; i < n; i+=numprocs) {
-
-
-       srand(i); 
-       x = ((double) rand()) / ((double) RAND_MAX);
-       y = ((double) rand()) / ((double) RAND_MAX);
-       //printf("%f  %f   rank: %d\n", x,y,rank); 
-       
-       z = sqrt((x*x)+(y*y));
-
-       
-       if(z <= 1.0)
+        srand(i); 
+        x = ((double) rand()) / ((double) RAND_MAX);
+        y = ((double) rand()) / ((double) RAND_MAX);
+        
+        z = sqrt((x*x)+(y*y));
+        if(z <= 1.0)
             count++;
     }
     
-
     pi = ((double) count/(double) n)*4.0;
-
     printf("proccess %d estimates that pi is: %f \n", rank, pi);
 
     MPI_Reduce(&pi, &piAux, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
 
-    if (rank == 0){
+    if (rank == 0)
         printf("pi is approx. %.16f, Error is %.16f\n", piAux, fabs(piAux - PI25DT));
-    }
         
     MPI_Finalize();
 }
