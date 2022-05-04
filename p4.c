0aa5c6f18bd1f7e101b312f3c85d29602edaf433
jfrozas <91458621+jfrozas@users.noreply.github.com>
2022-05-04 19:58:04 +0200

    Add files via upload

--- p4.c
+++ p4.c
@@ -14,13 +14,10 @@ int main(int argc, char *argv[] ) {
   float result[N], resultAux[N];
 
   int numprocs, rank;
-  int *sendcounts, *displs;
-  float rec_buf[N*N];
-  int a = 0;
-  int b = 3;
+  int *displs;
+  double rec_buf[N*N];
 
   int *rows;
-
 
   MPI_Status status;
 
@@ -28,24 +25,29 @@ int main(int argc, char *argv[] ) {
   MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
+
+  if(numprocs>N){
+    if(rank == 0){
+      printf("Numprocs cant be more than %d\n", N);
+
+    }
+    MPI_Finalize();
+    return 0;
+  }
+
   for(i=0; i<N; i++){
     result[i]=0;
   }
 
-
   rows = malloc(sizeof(int)*N);
   displs = malloc(sizeof(int)*N);
 
-
-  //MIRAR ESTOO
-  
   float rem = N%numprocs;
 
   for (int i = 0; i < numprocs; i++) {
       rows[i] = N/numprocs;
       if (rem > 0) {
           rows[i]++;
-          //printf("%d", rows[i]);
           rem--;
       }
 
@@ -54,62 +56,25 @@ int main(int argc, char *argv[] ) {
       rows[i] = rows[i]*N;
   } 
 
-
-
-  //Esto coge y asigna N/numprocs filas a cada proceso en rows [rank],
-    //y mete el remanente (rem) ordenadamente desde la primera
-
   if (rank == 0){
 
     for(i=0;i<N;i++) {
       vector[i] = i;
       for(j=0;j<N;j++) {
         matrix[i][j] = i+j;
-        printf("matrix[%d][%d] = %.f\n", i,j,matrix[i][j]);
       } 
     }
-  }
-
-  if (0 == rank) {
-      for (int i = 0; i < numprocs; i++) {
-          printf("rows[%d]*N = %d\tdispls[%d] = %d\n", i, rows[i], i, displs[i]);
-      }
   }
 
   MPI_Scatterv(&matrix, rows, displs, MPI_FLOAT, &rec_buf, N*rows[rank], MPI_FLOAT, 0, MPI_COMM_WORLD);
   MPI_Bcast(&vector, N, MPI_FLOAT, 0, MPI_COMM_WORLD);
 
-
-  MPI_Barrier(MPI_COMM_WORLD);
-  for (int i = 0; i < rows[rank]; i++) {
-    printf("rank: %d, received: %.f\n", rank,rec_buf[i]);
-
-  //  result[rank] += rec_buf[i];
-  //  printf("rank = %d, result = %f\n",rank, result[rank]);
-  }
-  MPI_Barrier(MPI_COMM_WORLD);
-
-  for(i=0; i<N;i++){
-    //printf("vector= %.f\n", vector[i]);
-  }
-  MPI_Barrier(MPI_COMM_WORLD);
-
   for (int i = 0; i < (rows[rank]/N); i++){
     for(j=i*N; j<(i+1)*N; j++){
-      //printf("rank:%d, i:%d, j:%d\n",rank, i, j);
       result[i] += rec_buf[j] * vector[j-(i*N)];
-
-      //printf("rank[%d], %f * %f, result[%d]: %f\n", rank, rec_buf[j], vector[j-(i*N)],  i, result[i]);
     }
   }
 
-
-
-  for(i=0; i<rows[rank]/N; i++){
-    printf("rank = %d, RESULT[%d] = %f\n",rank, i, result[i]);
-  }
-
-  //printf("\n");
   MPI_Barrier(MPI_COMM_WORLD);
 
   for(i = 0; i < numprocs; i++){
@@ -117,31 +82,23 @@ int main(int argc, char *argv[] ) {
     displs[i] = displs [i] / N;
   }
 
-
-
   MPI_Gatherv(&result, rows[rank], MPI_FLOAT, resultAux, rows, displs, MPI_FLOAT, 0, MPI_COMM_WORLD);
   MPI_Barrier(MPI_COMM_WORLD);
 
   if(rank == 0){
     if (DEBUG){
       for(i=0;i<N;i++) {
-        printf("ANSWER; %f \n",resultAux[i]);
+        printf("ANSWER: %.f \n",resultAux[i]);
       }
     }
   }
 
-  //MPI_Reduce(&result, &resultAux, numprocs, MPI_FLOAT, MPI_SUM, 0, MPI_COMM_WORLD);
-
   MPI_Barrier(MPI_COMM_WORLD);
-  //if (rank == 0){
-  //  for (int i = 0; i < numprocs; i++)
-  //    {
-  //      //printf("result[%d]: %f\n",i, resultAux[i]);
-  //    }
-  //}
-
 
   MPI_Finalize();
+
+  free(rows);
+  free(displs);
 
 
   return 0;
