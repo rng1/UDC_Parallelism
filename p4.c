010ffbae6f22e7350a9eafd6efd85ec3625b435b
jfrozas <91458621+jfrozas@users.noreply.github.com>
2022-05-04 21:20:49 +0200

    final(1022 peta)

--- p4.c
+++ p4.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <mpi.h>
+#include <sys/time.h>
 
 #define DEBUG 1
 
-#define N 6
+#define N 1022
 
 int main(int argc, char *argv[] ) {
 
@@ -16,15 +17,25 @@ int main(int argc, char *argv[] ) {
   int numprocs, rank;
   int *displs;
   float rec_buf[N*N];
-
   int *rows;
 
+  struct timeval  tvcomp1, tvcomp2, tvscatter1, tvscatter2, tvgather1, tvgather2;
+  int comm_time, comp_time;
 
   MPI_Status status;
 
   MPI_Init(&argc, &argv);
   MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+  if(numprocs>N){
+    if (rank == 0){
+      printf("ERROR: Execute the program with less than %d numprocs\n", N);
+    }
+    MPI_Finalize();
+    return 0;
+  }
+
 
   for(i=0; i<N; i++){
     result[i]=0;
@@ -33,9 +44,6 @@ int main(int argc, char *argv[] ) {
 
   rows = malloc(sizeof(int)*N);
   displs = malloc(sizeof(int)*N);
-
-
-  //MIRAR ESTOO
   
   float rem = N%numprocs;
 
@@ -60,9 +68,13 @@ int main(int argc, char *argv[] ) {
       } 
     }
   }
+  gettimeofday(&tvscatter1, NULL);
 
   MPI_Scatterv(&matrix, rows, displs, MPI_FLOAT, &rec_buf, N*rows[rank], MPI_FLOAT, 0, MPI_COMM_WORLD);
   MPI_Bcast(&vector, N, MPI_FLOAT, 0, MPI_COMM_WORLD);
+
+  gettimeofday(&tvscatter2, NULL);
+  gettimeofday(&tvcomp1, NULL);
 
   for (int i = 0; i < (rows[rank]/N); i++){
     for(j=i*N; j<(i+1)*N; j++){
@@ -71,20 +83,45 @@ int main(int argc, char *argv[] ) {
     }
   }
 
+  gettimeofday(&tvcomp2, NULL);
+  
+  int microseconds_scatter = (tvscatter2.tv_usec - tvscatter1.tv_usec)+ 1000000 * (tvscatter2.tv_sec - tvscatter1.tv_sec);
+  int microseconds_comp = (tvcomp2.tv_usec - tvcomp1.tv_usec)+ 1000000 * (tvcomp2.tv_sec - tvcomp2.tv_sec);  
+
   for(i = 0; i < numprocs; i++){
     rows[i] = rows[i] / N;
     displs[i] = displs [i] / N;
   }
 
-
+  gettimeofday(&tvgather1, NULL);
 
   MPI_Gatherv(&result, rows[rank], MPI_FLOAT, resultAux, rows, displs, MPI_FLOAT, 0, MPI_COMM_WORLD);
   MPI_Barrier(MPI_COMM_WORLD);
 
+  gettimeofday(&tvgather2, NULL);
+
+  int microseconds_gather = (tvgather2.tv_usec - tvgather1.tv_usec)+ 1000000 * (tvgather2.tv_sec - tvgather1.tv_sec);
+  int microseconds_comm = microseconds_scatter + microseconds_gather; 
+  
+
+  if (rank != 0) {
+        MPI_Send(&microseconds_comm, 1, MPI_INT, 0, 1, MPI_COMM_WORLD);
+        MPI_Send(&microseconds_comp, 1, MPI_INT, 0, 1, MPI_COMM_WORLD);
+  } else {
+        for (int i = 1; i < numprocs; ++i) {
+            MPI_Recv(&comm_time, 1, MPI_INT, i, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+            MPI_Recv(&comp_time, 1, MPI_INT, i, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+            printf("Comm Time (seconds) [%d] = %lf\n", i, (double) comm_time/1E6);
+            printf("Comp Time (seconds) [%d] = %lf\n", i, (double) comp_time/1E6);
+        }
+        printf("Comm Time (seconds) [0] = %lf\n", (double) microseconds_comm/1E6);
+        printf("Comp Time (seconds) [0] = %lf\n", (double) microseconds_comp/1E6);
+  }     
+
   if(rank == 0){
     if (DEBUG){
       for(i=0;i<N;i++) {
-        printf("ANSWER; %f \n",resultAux[i]);
+        printf("ANSWER; %.f \n",resultAux[i]);
       }
     }
   }
